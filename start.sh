999664116a8353e002f166a880d6fb9768c0efe7
Apondi-Otieno <apondiotii@gmail.com>
2022-03-15 11:25:36 +0300

    mstart.sh  file

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
+export SECRET_KEY='mynameischebaibai'
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:1234@localhost/bblog'
+export MAIL_USERNAME="apondoti@gmail.com"
+export MAIL_PASSWORD='34850829amk'
+
+
 python3 manage.py server
