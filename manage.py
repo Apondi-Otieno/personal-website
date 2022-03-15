3b3e1c1e9cd46c5584b7c28f88e972bb71c5d904
Apondi-Otieno <apondiotii@gmail.com>
2022-03-15 11:38:41 +0300

    manage.py file

--- manage.py
+++ manage.py
@@ -2,3 +2,25 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
 from app.models import User,Blog, Comment,Subscriber
+
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Blog = Blog,Comment = Comment,Subscriber = Subscriber)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == "__main__":
+    manager.run()
