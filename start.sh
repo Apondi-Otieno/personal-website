68c739b9e4ecb7451025440a0b744de6ad5e6541
Apondi-Otieno <apondiotii@gmail.com>
2022-03-15 11:38:07 +0300

    manage.py file

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='mynameischebaibai'
-export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:1234@localhost/bblog'
+# export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:1234@localhost/bblog'
 export MAIL_USERNAME="apondoti@gmail.com"
 export MAIL_PASSWORD='34850829amk'
 
