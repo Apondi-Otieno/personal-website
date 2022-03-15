7b62f97d61d8b10e28713e1bfa6c670482be9531
Apondi-Otieno <apondiotii@gmail.com>
2022-03-15 11:39:47 +0300

    this will delete init.py imports

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-import os  
-from flask import Flask
-from config import config_options
-from flask_mail import Mail
-from flask_login import LoginManager, login_manager
-from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
-from flask_uploads import IMAGES, UploadSet,configure_uploads
