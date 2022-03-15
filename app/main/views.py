db872ac24ca25fcd4c341439b378c54e6160346a
Apondi-Otieno <apondiotii@gmail.com>
2022-03-15 13:53:02 +0300

    this will add imports

---
+++ app/main/views.py
@@ -0,0 +1,12 @@
+from flask import render_template, redirect,url_for,abort,request
+from . import main
+from app.requests import get_quote
+from flask_login import login_required,current_user
+from ..models import User,Blog,Comment,Subscriber
+from ..import db, photos
+import secrets
+import os
+from PIL import Image
+from .forms import UpdateProfile,CreateBlog
+from ..email import mail_message
+
